297d6bc1e2eb0515d976cc656dd43f4714adaf4c
kubas8111 <kubas8111@interia.pl>
2024-03-19 12:33:16 +0100

    Initial commit

---
+++ src/test/java/com/example/adwise/AdwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
