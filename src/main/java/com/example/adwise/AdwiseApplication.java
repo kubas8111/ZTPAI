5a56d8eaba5cc1d24291c22ca1cb4236b37015be
kubas8111 <kubas8111@interia.pl>
2024-04-29 14:43:19 +0200

    small changes

--- src/main/java/com/example/adwise/AdwiseApplication.java
+++ src/main/java/com/example/adwise/AdwiseApplication.java
@@ -1,12 +1,8 @@
 package com.example.adwise;
 
-import com.example.adwise.entities.Profile;
-import com.example.adwise.repositories.ProfileRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,22 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class AdwiseApplication {
-
-//	private void insertProfiles(ProfileRepository profileRepository) {
-//		profileRepository.save(new Profile(null,"a","a","a","a","a", (short) 0));
-//		profileRepository.save(new Profile(null,"b","b","b","a2","a", (short) 1));
-//		profileRepository.save(new Profile(null,"c","c","c","a3","a", (short) 0));
-//		profileRepository.save(new Profile(null,"d","d","d","a4","a", (short) 0));
-//	}
-//
-//	@Bean
-//	public CommandLineRunner run(ProfileRepository profileRepository) {
-//		return (args) -> {
-//			insertProfiles(profileRepository);
-//			System.out.println(profileRepository.findAll());
-//		};
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdwiseApplication.class, args);
 	}
--- src/main/java/com/example/adwise/loader/LoadDatabase.java
+++ src/main/java/com/example/adwise/loader/LoadDatabase.java
@@ -2,14 +2,9 @@ package com.example.adwise.loader;
 
 import com.example.adwise.entities.Profile;
 import com.example.adwise.repositories.ProfileRepository;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.Logger;
 
 @Component
 public class LoadDatabase implements CommandLineRunner {
@@ -28,18 +23,4 @@ public class LoadDatabase implements CommandLineRunner {
              this.profileRepository.save(new Profile(null, "user@user.com", passwordEncoder.encode("user"), "user", "u", "11234123", false));
          }
      }
-
-
-//    private static final Logger log = (Logger) LoggerFactory.getLogger(LoadDatabase.class);
-//
-//    @Bean
-//    CommandLineRunner initDatabase(ProfileRepository profileRepository) {
-//
-//        return args -> {
-//            log.info("Preloading " + profileRepository.save(new Profile(null,"a","a","a","a","a", (short) 0)));
-//            log.info("Preloading " + profileRepository.save(new Profile(null,"b","b","b","a2","a", (short) 1)));
-//            log.info("Preloading " + profileRepository.save(new Profile(null,"c","c","c","a3","a", (short) 0)));
-//            log.info("Preloading " + profileRepository.save(new Profile(null,"d","d","d","a4","a", (short) 0)));
-//        };
-//    }
 }
